e6c6c702e74f5f41a61b87abefcf02d7b86392ab
lafie-rage <corentin.destrez62@gmail.com>
2024-11-10 12:15:10 +0100

    [Jour 2 - Matin] Intents - Initialisation de l'UI

---
+++ app/src/main/java/com/example/tp_ecommerce_formation_android/ui/page/product/details/ProductDetailsActivity.kt
@@ -0,0 +1,15 @@
+package com.example.tp_ecommerce_formation_android.ui.page.product.details
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.tp_ecommerce_formation_android.R
+
+class ProductDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_product_details)
+    }
+}
