1e64ef1a13fe923bdbe23f6bbd80900ac8e8fa84
PCDZ06221 <ext.corentin.destrez@reseau.sncf.fr>
2024-11-19 08:30:16 +0100

    [Jour 2 - Matin] Fragment Manager - Gestion back pressed

--- app/src/main/java/com/example/tp_ecommerce_formation_android/ui/page/MainActivity.kt
+++ app/src/main/java/com/example/tp_ecommerce_formation_android/ui/page/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.tp_ecommerce_formation_android.ui.page
 
 import android.os.Bundle
+import androidx.activity.addCallback
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.tp_ecommerce_formation_android.R
 import com.example.tp_ecommerce_formation_android.databinding.ActivityMainBinding
 import com.example.tp_ecommerce_formation_android.ui.page.account.AccountFragment
@@ -34,11 +36,23 @@ class MainActivity : AppCompatActivity() {
                 else -> HomeFragment()
             }
             if (currentFragment == null || currentFragment::class.java != destination::class.java) {
-                supportFragmentManager.beginTransaction()
-                    .replace(binding.fragmentContainerView.id, destination)
-                    .commit()
+                navigateToDestination(destination)
             }
             return@setOnItemSelectedListener true
         }
+
+        onBackPressedDispatcher.addCallback(this) {
+            if (supportFragmentManager.findFragmentById(binding.fragmentContainerView.id) is HomeFragment) {
+                finish()
+            } else {
+                navigateToDestination(HomeFragment())
+            }
+        }
+    }
+
+    private fun navigateToDestination(destination: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(binding.fragmentContainerView.id, destination)
+            .commit()
     }
 }
